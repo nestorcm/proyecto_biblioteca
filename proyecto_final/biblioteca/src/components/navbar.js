c32da59f5d6ea231075cfc89e5afca79e82c6945
nestor <nestor.mandon@gmail.com>
2024-10-20 20:45:33 -0500

    logout btn

--- proyecto_final/biblioteca/src/components/navbar.js
+++ proyecto_final/biblioteca/src/components/navbar.js
@@ -1,26 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/UPB.jpeg";
 import "../assets/css/navbar.css";
 
-const navbar = () => {
+const Navbar = () => {
+  const handleLogout = () => {
+    // Lógica para cerrar sesión
+    console.log("Logout");
+  };
+
   return (
-    <div>
-      <nav className="navbar ">
-        <div className="container-fluid">
-          <Link to={"/"} className="navbar-brand">
-            <img
-              src={logo}
-              alt="Logo"
-              width={60}
-              height={25}
-              className="d-inline-block align-text-top"
-            />
-          </Link>
-        </div>
-      </nav>
-    </div>
+      <div>
+        <nav className="navbar">
+          <div className="container-fluid d-flex justify-content-between align-items-center">
+
+            <Link to={"/"} className="navbar-brand">
+              <img
+                  src={logo}
+                  alt="Logo"
+                  className="logo"
+              />
+            </Link>
+            <button className="btn btn-light" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        </nav>
+      </div>
   );
 };
 
-export default navbar;
+export default Navbar;
+
